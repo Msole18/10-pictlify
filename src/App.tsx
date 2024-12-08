0119dc2fe3a3bee33a8727758127e5dbdd0f4cc6
Msole18 <miguel.sole18@gmail.com>
2024-12-08 02:37:17 -0300

    Set up React Router: created routes, and components for each route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import './globals.css'
+import { Route, Routes } from 'react-router-dom'
+import SigninForm from './_auth/forms/SignInForm'
+import SignUpForm from './_auth/forms/SignUpForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
+import { Home } from './_root/pages'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello Pictlify!
-    </h1>
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sing-in" element={<SigninForm />} />
+          <Route path="/sing-up" element={<SignUpForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
