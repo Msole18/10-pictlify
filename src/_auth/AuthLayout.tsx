a442acfe7dfa3d339f3684e70ff2274b971f790b
Msole18 <miguel.sole18@gmail.com>
2024-12-09 05:57:13 -0300

    upgrade AuthLayput and add assets data

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,19 +1,25 @@
-import { Navigate, Outlet } from "react-router-dom"
+import { Navigate, Outlet } from 'react-router-dom'
 
 const AuthLayout = () => {
   const isAuthenticated = false
 
   return (
     <>
-      {isAuthenticated ? ( 
-        <Navigate to='/' /> 
-      ) : ( 
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
         <>
-        <section>
-          <Outlet />
-        </section>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+          
+          <img
+            alt="a logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+            src="/assets/images/side-img.svg"
+          />
         </>
-      ) }
+      )}
     </>
   )
 }
