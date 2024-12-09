464f84beb2ab607ee3fbd1c5cc8d06b09c0009a4
Msole18 <miguel.sole18@gmail.com>
2024-12-09 09:11:18 -0300

    add de sign up form

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -1,12 +1,11 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
+import { useForm, Control } from 'react-hook-form'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -14,9 +13,36 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { SignUpValidation } from '@/lib/validation'
+import { INewUser } from '@/lib/types/types'
+import Loader from '@/components/shared/Loader'
+import { Link } from 'react-router-dom'
 
+interface Props {
+  name: string
+  type: keyof INewUser
+  form: Control<z.infer<typeof SignUpValidation>>
+}
+
+const FieldForm = ({ name, type, form }: Props) => {
+  return (
+    <FormField
+      control={form}
+      name={type}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{name}</FormLabel>
+          <FormControl>
+            <Input className="shad-input" type={type} {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
 
 const SignUpForm = () => {
+  const isLoading = false
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignUpValidation>>({
     resolver: zodResolver(SignUpValidation),
@@ -24,37 +50,54 @@ const SignUpForm = () => {
       name: '',
       username: '',
       email: '',
-      password:'',
+      password: '',
     },
   })
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof SignUpValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+    // const newUser = await createUserAccount(values)
+    
   }
   return (
+    // <div className="flex flex-col justify-center items-center">
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="logo" />
+
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Create a new account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use Pictlify, Please enter your details
+        </p>
+
+        <form className="flex flex-col gap-3 w-full mt-2">
+          <FieldForm type="name" name="Name" form={form.control} />
+          <FieldForm type="username" name="Username" form={form.control} />
+          <FieldForm type="email" name="Email" form={form.control} />
+          <FieldForm type="password" name="Password" form={form.control} />
+
+          <Button type="submit" className="shad-button_primary">
+            {isLoading ? (
+              <div className="flex-center gap-2">
+                <Loader /> Loading...
+              </div>
+            ) : (
+              'Sign Up'
+            )}
+          </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account?
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-1"
+            >
+              Log in
+            </Link>
+          </p>
+        </form>
+      </div>
     </Form>
   )
 }
