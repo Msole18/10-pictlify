4b675ebe52d5051b2f49e634656aa059095e07c7
Msole18 <miguel.sole18@gmail.com>
2024-12-18 21:59:32 -0300

    create AllUsers page

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,6 +1,50 @@
+import { Loader } from '@/components/shared/Loader'
+import { UserCard } from '@/components/shared/UserCard'
+import { GET_TOP_CREATOR_USERS_LIMIT } from '@/constants'
+import { useToast } from '@/hooks/use-toast'
+import { useGetCreatorUsers } from '@/lib/react-query/queries'
+import { Models } from 'appwrite'
 
 export const AllUsers = () => {
+  const { toast } = useToast()
+
+  const {
+      data: creators,
+      isPending: isCreatorloading,
+      isError: isErrorCreators,
+    } = useGetCreatorUsers(GET_TOP_CREATOR_USERS_LIMIT)
+
+  if (isErrorCreators) {
+    toast({ title: 'Something went wrong.' })
+
+    return
+  }
+
   return (
-    <div>AllUsers</div>
+    <div className="common-container">
+      <div className="user-container">
+        <div className='flex gap-2'>
+
+        <img
+          src={'/assets/icons/people.svg'}
+          alt="people"
+          width={20}
+          height={20}
+        />
+        <h2 className="h3-bold md:h2-bold text-left w-full">All Users</h2>
+        </div>
+        {isCreatorloading && !creators ? (
+          <Loader />
+        ) : (
+          <ul className="user-grid">
+            {creators?.documents.map((creator: Models.Document) => (
+              <li key={creator.$id} className="flex-1 min-w-[200px] w-full">
+                <UserCard user={creator} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
   )
 }
